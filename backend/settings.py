3b3b86b853887b743970c3f5342e1d1823ed4d83
ayush02av <ayush02av@gmail.com>
2021-12-26 22:23:18 +0530

    Added postgres aws db

--- backend/settings.py
+++ backend/settings.py
@@ -13,15 +13,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rvrjhf1pp$sh-p-_pe=y&l8oo*@3bx(c&$6k@6r-h#b!4t-l@'
+# SECRET_KEY = 'django-insecure-8rvrjhf1pp$sh-p-_pe=y&l8oo*@3bx(c&$6k@6r-h#b!4t-l@'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,11 +90,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD':os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
